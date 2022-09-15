88799bfa82a4b753f84b1748a602fc05d3dc7795
Carlos <99026597+CMCCVR@users.noreply.github.com>
2022-09-15 18:58:35 -0300

    inclusçao de bandeiras

--- src/App.js
+++ src/App.js
@@ -2,33 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  const [user, setUser] = useState({
-    id: 1,
-    pais: "Republica de Cabo Verde",
-    photo:
-      "https://viajecomnorma.com.br/wp-content/uploads/2019/11/Mapa-Cabo-Verde.jpg",
-  });
-
-  const objs = 
-
+  const [pais, setPais] = useState("");
   const [valor, setValor] = useState(0);
-
   function FuncDiminuir() {
     setValor(valor - 1);
   }
   function FuncAumentar() {
-    setValor(valor + 1);
-  }
+    setValor(valor + 1);  }
 
   return (
     <div className="App">
-      <h2>Nome: {name}</h2>
+      <h2>Nome: {pais}</h2>
       <input
         type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        value={pais}
+        onChange={(e) => setPais(e.target.value)}
       />
+    
       <div>
         <p>
           Número: <b>{valor}</b>
@@ -36,9 +26,7 @@ function App() {
         <button onClick={FuncAumentar}>Aumentar número!</button>
         <button onClick={FuncDiminuir}>Diminuir número!</button>
         <hr />
-        Nacionalidade: <b>{user.pais}</b>
-        <br />
-        <img src={user.photo} />
+        <img src={`https://countryflagsapi.com/svg/${pais}`} />
       </div>
     </div>
   );
